9eb97539e8707ba2d62eca02e6ca9f3908cfdb1f
Angela Ti <tas14@yandex.ru>
2017-04-02 19:32:03 +0300

    попробовала js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,37 @@
       navMain.classList.remove("menu--open");
     }
   });
+
+var link = document.querySelector(".button--product");
+var popup = document.querySelector(".modal__form");
+
+var shadow = document.querySelector(".modal__overlay");
+var input = popup.querySelector("[type=radio]");
+
+
+// Открытие окна
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.add("modal");
+    popup.classList.add("modal__show");
+  /*  shadow.classList.add("modal__overlay-shadow");*/
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+        login.focus();
+    }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal__content-show")) {
+      popup.classList.remove("modal__content-show");
+    }
+  }
+});
+
+shadow.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal__content-show");
+});
